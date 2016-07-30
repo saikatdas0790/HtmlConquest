440f532dc8c5d75606bf3ae45f220fc4a0fb9496
Swashata Ghosh <swashata4u@gmail.com>
2016-07-30 18:09:46 +0530

    Fix issue with the radio element
    We needed multiple radio elements to make some sense out of the form.
    Also the naming was at fault. Check the inline comments.

--- HTML Tutorial/09 - HTML Forms/forms-exercise.php
+++ HTML Tutorial/09 - HTML Forms/forms-exercise.php
@@ -27,15 +27,51 @@
 			</pre>
 		<?php endif; ?>
 		<form action="" method="POST">
-			<label for="string1">String 1</label>
-			<input type="text" name="string1" id="string1" value="User Data"><br>
+			<p>
+				<label for="string1">String 1</label>
+				<input type="text" name="string1" id="string1" value="User Data">
+			</p>
 
-			<label for="arr1[v1]">Array 1 V1</label>
-			<input type="text" name="arr1[v1]" id="arr1_v1" value="User Data"><br>
+			<p>
+				<label for="arr1[v1]">Array 1 V1 (arr1[v1])</label>
+				<input type="text" name="arr1[v1]" id="arr1_v1" value="User Data">
+			</p>
+
+			<!-- We use fieldset to group a set of radios/checkboxes
+			Actually we don't use fieldset at all! Just for the sake of it -->
+			<fieldset>
+				<!-- And don't forget the legend -->
+				<legend>Radio Elements (arr1[arr2][0]</legend>
+				<!-- I though you'd understand, I wasn't talking about just one radio button
+				I was talking about multiple radio, grouped under the same name
+				Having just one radio under a single name, doesn't make any sense, because
+				user won't be able to deselect once selected. What would we need it for?
+
+				Also you named the element arr1[arr2][], which would render arr1[arr2][0]
+				because of it's position. I don't know if you were being smart or ignorant,
+				but it is not the right way of doing. In future, if we are to move form elements
+				then it would break the indicies. This is not something we want.
+
+				Because it is a radio element ( always an implied string datatype ) we can just
+				hardcode its name
+				-->
+				<input type="radio" name="arr1[arr2][0]" id="arr1_arr2_0_1" value="1" />
+				<!-- The for attribute of the <label> element corresponds to the id of the form element
+				NOT the name of the form element -->
+				<label for="arr1_arr2_0_1">Value 1</label><br />
+
+				<!-- Similarly -->
+				<input type="radio" name="arr1[arr2][0]" id="arr1_arr2_0_2" value="2" />
+				<label for="arr1_arr2_0_2">Value 2</label><br />
+
+				<input type="radio" name="arr1[arr2][0]" id="arr1_arr2_0_3" value="3" />
+				<label for="arr1_arr2_0_3">Value 3</label><br />
+
+				<input type="radio" name="arr1[arr2][0]" id="arr1_arr2_0_4" value="4" />
+				<label for="arr1_arr2_0_4">Value 4</label><br />
+			</fieldset>
 
 			<fieldset>
-				<label for="arr1[arr2][0]">Array 1 Array 2 0th Element</label>
-				<input type="radio" name="arr1[arr2][]" id="arr1_arr2_0" value="1"><br>
 
 				<label for="arr1[arr2][1][0]">Array 1 Array 2 1st Element 0th Element</label>
 				<input type="checkbox" name="arr1[arr2][1][]" id="arr1_arr2_1_0" value="0"><br>
