b77dae0a12e97cc2099abe88c14a641cc1a8bd4f
Swashata Ghosh <swashata4u@gmail.com>
2016-08-08 01:22:56 +0530

    Add working example with some interactivity for CSS Reset
    We add a nice green button on the CSS reset example page, to show
    (live) how CSS reset affects the page. This also (if you feel interested)
    gives you a hint of how interactivity is added to a page

--- CSS Tutorials/02 CSS Reset/app.js
+++ CSS Tutorials/02 CSS Reset/app.js
@@ -1,6 +1,30 @@
 ( function() {
 	"use strict";
 
-	// Add some functionality to the reset button
-	var resetButton = document.getElementById( 'css-reseter' );
+	// Execute the APP when DOM is ready
+	document.addEventListener( 'DOMContentLoaded', function() {
+		// Add some functionality to the reset button
+		var resetButton = document.getElementById( 'css-reseter' ),
+		resetCSS = document.getElementById( 'reset-css' );
+		resetButton.onclick = function() {
+			// Get the attribute
+			var currentlyDisabled = resetCSS.getAttribute( 'disabled' );
+
+			// If it is already enabled
+			if ( null === currentlyDisabled ) {
+				// Change the text
+				resetButton.innerHTML = 'Enable CSS Reset';
+
+				// Add the disabled attribute
+				resetCSS.setAttribute( 'disabled', 'disabled' );
+			} else {
+				// Change the text
+				resetButton.innerHTML = 'Disable CSS Reset';
+
+				// Remove the disabled attribute
+				resetCSS.removeAttribute( 'disabled' );
+			}
+		};
+	});
+
 } )();
